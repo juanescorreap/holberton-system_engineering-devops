220842a8155397a417c40e0eaef030f596495bc2
John Doe <juanestebancope@outlook.com>
2022-01-31 14:48:55 -0500

    README.md and task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import requests
+import sys
+if __name__ == "__main__":
+
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1])).json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    user_todos = 0
+    completed = 0
+    todos_list = []
+
+
+    for i in todos:
+        if i.get('userId') == user.get('id'):
+            user_todos = user_todos + 1
+            if i.get('completed') == True:
+                completed = completed + 1
+                todos_list.append(i)
+
+    print("Employee {} is done with tasks({}/{}):".format(user.get('name'), completed, user_todos))
+    for j in todos_list:
+        print("\t{}".format(j.get('title')))
+
---
+++ 0x15-api/README.md
@@ -0,0 +1,18 @@
+# 0x15. API
+
+Project developed by Juan Esteban Correa Pérez as part of the foundations program at Holberton School.
+
+It's purpose is to help the student understand:
+
+What Bash scripting should not be used for<br />
+What is an API<br />
+What is a REST API<br />
+What are microservices<br />
+What is the CSV format<br />
+What is the JSON format<br />
+Pythonic Package and module name style<br />
+Pythonic Class name style<br />
+Pythonic Variable name style<br />
+Pythonic Function name style<br />
+Pythonic Constant name style<br />
+Significance of CapWords or CamelCase in Python<br />
