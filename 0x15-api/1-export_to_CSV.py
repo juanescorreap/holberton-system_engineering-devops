7072f698c1da84af85555962976196c9591123cd
John Doe <juanestebancope@outlook.com>
2022-01-31 15:55:45 -0500

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import csv
+import requests
+import sys
+if __name__ == "__main__":
+
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(sys.argv[1])).json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    user_todos = 0
+    completed = 0
+    todos_list = []
+    tasks_list = []
+
+    for i in todos:
+        if i.get('userId') == user.get('id'):
+            user_todos = user_todos + 1
+            tasks_list.append(i)
+            if i.get('completed') is True:
+                completed = completed + 1
+                todos_list.append(i)
+
+    print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
+          completed, user_todos))
+    for j in todos_list:
+        print("\t{}".format(j.get('title')))
+
+    username = user.get('username')
+    f = open('{}.csv'.format(sys.argv[1]), 'w')
+    try:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerow(
+            ("USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"))
+        for k in tasks_list:
+            writer.writerow((k.get('userId'), username,
+                            k.get('completed'), k.get('title')))
+    finally:
+        f.close()
