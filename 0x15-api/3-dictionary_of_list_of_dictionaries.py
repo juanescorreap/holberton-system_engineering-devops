2d199dca9d6777e34a0094586c9414f6609a1cb5
John Doe <juanestebancope@outlook.com>
2022-02-01 12:22:54 -0500

    identation and pep8

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
-""" some word going here """
+"""
+Script to export data in the  JSON format
+"""
 
 import json
 import requests
 
+
 if __name__ == "__main__":
-    print('Vaca')
+
+    user = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    todos_list = []
+    user_dict = {}
+    tasks_dict = {}
+
+    for j in user:
+        for i in todos:
+            if i.get('userId') == j.get('id'):
+                tasks_dict = {"username": j.get('username'),
+                              "task": i.get('title'),
+                              "completed": i.get('completed')}
+                todos_list.append(tasks_dict)
+        user_dict[j.get('id')] = todos_list
+
+        with open('todo_all_employees.json', 'w') as f:
+            json.dump(user_dict, f)
