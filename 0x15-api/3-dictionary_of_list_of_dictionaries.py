4fa23cd25794a896f58db68fb97169f2d851aede
John Doe <juanestebancope@outlook.com>
2022-02-01 12:15:44 -0500

    test

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,26 +1,8 @@
 #!/usr/bin/python3
-"""
-Script to export data in the  JSON format
-"""
+""" some word going here """
+
 import json
 import requests
+
 if __name__ == "__main__":
-
-    user = requests.get('https://jsonplaceholder.typicode.com/users').json()
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-
-    todos_list = []
-    user_dict = {}
-    tasks_dict = {}
-
-for j in user:
-    for i in todos:
-        if i.get('userId') == j.get('id'):
-            tasks_dict = {"username": j.get('username'),
-                          "task": i.get('title'),
-                          "completed": i.get('completed')}
-            todos_list.append(tasks_dict)
-    user_dict[j.get('id')] = todos_list
-
-    with open('todo_all_employees.json', 'w') as f:
-        json.dump(user_dict, f)
+    print('Vaca')
