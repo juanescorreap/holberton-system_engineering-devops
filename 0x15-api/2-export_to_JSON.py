11522d75ec8a0ee08350dd6788f871bb1f7d9315
John Doe <juanestebancope@outlook.com>
2022-01-31 19:17:51 -0500

    delete coma at the end of line 22

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,17 +12,16 @@ if __name__ == "__main__":
     todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
 
     todos_list = []
-    tasks_dict = {}
     user_dict = {}
-    username = user.get('username')
+    tasks_dict = {}
 
     for i in todos:
         if i.get('userId') == user.get('id'):
             tasks_dict = {"task": i.get('title'),
                           "completed": i.get('completed'),
-                          "username": username},
+                          "username": user.get('username')}
             todos_list.append(tasks_dict)
-    user_dict[sys.argv[1]] = todos_list
+    user_dict = {sys.argv[1]: todos_list}
 
     with open('{}.json'.format(sys.argv[1]), 'w') as f:
         json.dump(user_dict, f)
