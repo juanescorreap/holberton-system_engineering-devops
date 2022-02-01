b4a204e66f4c0509edcc89d8941d86546b13fd0f
John Doe <juanestebancope@outlook.com>
2022-01-31 19:41:14 -0500

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script to export data in the CSV format
+"""
+import json
+import requests
+import sys
+if __name__ == "__main__":
+
+    user = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    todos_list = []
+    user_dict = {}
+    tasks_dict = {}
+
+for j in user:
+    for i in todos:
+        if i.get('userId') == j.get('id'):
+            tasks_dict = {"username": j.get('username'),
+                          "task": i.get('title'),
+                          "completed": i.get('completed')}
+            todos_list.append(tasks_dict)
+    user_dict[j.get('id')] = todos_list
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(user_dict, f)
