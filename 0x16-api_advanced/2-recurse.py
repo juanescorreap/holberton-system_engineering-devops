c88e275b45566758cd92179d64b924437c5ac963
John Doe <juanestebancope@outlook.com>
2022-02-17 22:29:12 -0500

    pep8

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,11 +3,10 @@
 Write a recursive function that queries the Reddit API and returns
 a list containing the titles of all hot articles for a given subreddit
 """
-from urllib import request
-import requests
 
 
 def recurse(subreddit, hot_list=[], count=0, after=None):
+    import requests
     user = {"User-Agent": "mozilla"}
     params = {"count": count, "after": after}
     request = requests.get('https://www.reddit.com/r/{}/hot.json'
