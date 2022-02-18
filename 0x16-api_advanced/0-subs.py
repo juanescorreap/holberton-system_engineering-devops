b16384113a5a049e4a4937dd40f6f222daccee20
John Doe <juanestebancope@outlook.com>
2022-02-17 20:46:21 -0500

    README file and task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns
+the number of subscribers for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    request = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.format(subreddit),
+        allow_redirects=False).json()
+    if request.status_code == 200:
+        return request.get('data').get('subscribers')
+    else:
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,11 @@
+# 0x16. API advanced
+
+Project developed by Juan Esteban Correa Pérez as part of the foundations program at Holberton School.
+
+It's purpose is to help the student understand:
+
+How to read API documentation to find the endpoints you’re looking for<br />
+How to use an API with pagination<br />
+How to parse JSON results from an API<br />
+How to make a recursive API call<br />
+How to sort a dictionary by value<br />
