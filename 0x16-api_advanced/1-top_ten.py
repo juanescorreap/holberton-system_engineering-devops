3526929e1680ff55c238327206c0c327ae9bf2fe
John Doe <juanestebancope@outlook.com>
2022-02-17 21:43:19 -0500

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
+
+def top_ten(subreddit):
+    import requests
+    import json
+    request = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'.
+                           format(subreddit), allow_redirects=False)
+    if request.status_code == 404:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in request.json().get("data").get("children")]
