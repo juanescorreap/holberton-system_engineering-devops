e0cbd55862d603fcb74af1e39635916350f07efb
John Doe <juanestebancope@outlook.com>
2022-02-17 21:50:12 -0500

    documentation

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,10 +3,14 @@
 function that queries the Reddit API and prints the titles
 of the first 10 hot posts listed for a given subreddit.
 """
-import requests
 
 
 def top_ten(subreddit):
+    """
+    function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
+    """
+    import requests
     request = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'.
                            format(subreddit), allow_redirects=False)
     if request.status_code == 404:
